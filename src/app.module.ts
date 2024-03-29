9a2961b8407b3890709dd3d99632c485027d689f
tri <devtrantrongtri@gmail.com>
2024-03-29 23:52:46 +0700

    connected mongo, configserver .env, create user schema,createUser function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // config mongoose module
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    // configservice for dotenv
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // users module
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,23 @@
+// export class User {
+
+
+
+// }
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop({required: true})
+  email: string;
+
+  @Prop({required: true})
+  password : string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
